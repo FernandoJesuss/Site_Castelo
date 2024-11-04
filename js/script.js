3c4e13bc2b87caea035001aef251cc9c710c1e59
fernandoJesuss <fernando_ffernando@hotmail.com>
2024-11-04 19:47:54 -0300

    style:tualização/código sem impacto no significado

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("infoButton").onclick = function() {
+    var infoText = document.getElementById("infoText");
+    infoText.style.display = infoText.style.display === "none" ? "block" : "none";
+};
+
+document.getElementById("audioButton").onclick = function() {
+    var video = document.getElementById("backgroundVideo");
+    video.muted = !video.muted;
+    this.textContent = video.muted ? "Ativar Áudio" : "Desativar Áudio";
+};
