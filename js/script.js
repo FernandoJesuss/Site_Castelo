521d72b441617eb3e96010fbb1d8a0a7b40af72c
fernandoJesuss <fernando_ffernando@hotmail.com>
2024-11-08 00:02:49 -0300

    Refactor:Melhorias no codigo

--- js/script.js
+++ js/script.js
@@ -23,7 +23,16 @@ hamburger.addEventListener('click', () => {
 });
 
 
+// Uncheck radio buttons (unfortunatley this cannot be done with css alone)
 
+document.querySelectorAll('input[type="radio"]').forEach((radio) => {
+    radio.addEventListener("click", (e) => {
+      e.preventDefault();
+      // setting a timeout enables this hack to work
+      setTimeout(() => (radio.checked = !radio.checked), 0);
+    });
+  });
+  
 
 
 
