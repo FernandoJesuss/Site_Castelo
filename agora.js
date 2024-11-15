65fcf50afaea33c622088cf1b24c12ba358666e8
fernandoJesuss <fernando_ffernando@hotmail.com>
2024-11-14 23:56:49 -0300

    Fix: Correção de bugs

--- agora.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.personagem-container').forEach(container => {
-  container.addEventListener('click', () => {
-    document.querySelectorAll('.personagem-container').forEach(c => c.classList.remove('active'));
-    container.classList.add('active');
-  });
-});
--- css/test.js
+++
@@ -1,9 +0,0 @@
-// Uncheck radio buttons (unfortunatley this cannot be done with css alone)
-
-document.querySelectorAll('input[type="radio"]').forEach((radio) => {
-  radio.addEventListener("click", (e) => {
-    e.preventDefault();
-    // setting a timeout enables this hack to work
-    setTimeout(() => (radio.checked = !radio.checked), 0);
-  });
-});
